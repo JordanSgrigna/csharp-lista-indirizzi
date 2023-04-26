d66b69e3354ed432f0cd6e6a0195a98dd2650cba
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-26 14:40:16 +0200

    Add project files.

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+	public class Address
+	{
+		//ATTRIBUTES + PROPERTIES
+		private string name;
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				this.name = value;
+			}
+		}
+
+		private string surname;
+		public string Surname
+		{
+			get
+			{
+				return this.surname;
+			}
+			set
+			{
+				this.surname = value;
+			}
+		}
+
+		private string street;
+		public string Street
+		{
+			get
+			{
+				return this.address;
+			}
+		}
+
+		private string city;
+		public string City
+		{
+			get
+			{
+				return this.city;
+			}
+		}
+
+		private string country;
+		public string Country
+		{
+			get
+			{
+				return this.country;
+			}
+		}
+
+		private string province;
+		public string Province
+		{
+			get
+			{
+				return this.province;
+			}
+		}
+
+		private int zip;
+		public int Zip
+		{
+			get
+			{
+				return this.zip;
+			}
+		}
+
+		//CONSTRUCTOR
+		public Address(string name, string surname, string street, string city, string country, string province, int zip)
+		{
+			this.name = name;
+			this.surname = surname;
+			this.street = street;
+			this.city = city;
+			this.country = country;
+			this.province = province;
+			this.zip = zip;
+
+		}
+
+
+
+
+
+	}
+
+	
+	
+}
---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,23 @@
+﻿using csharp_lista_indirizzi;
+
+List<Address> address = new List<Address>();
+
+StreamReader addressesFile = File.OpenText("C:\\Users\\jorda\\Desktop\\Generation\\Esercizi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\data\\addresses.csv");
+int lineCounter = 0;
+
+while (!addressesFile.EndOfStream)
+{
+	string textLinesRead = addressesFile.ReadLine();
+	lineCounter++;
+	
+	if(lineCounter > 1)
+	{
+		string[] separatedLine = textLinesRead.Split(',');
+		int zipAddressRead = int.Parse(separatedLine[6]);
+
+		Address AddressRead = new Address(separatedLine[0], separatedLine[1], separatedLine[2], separatedLine[3], separatedLine[4], separatedLine[5], zipAddressRead);
+		address.Add(AddressRead);
+	}
+}
+
+
